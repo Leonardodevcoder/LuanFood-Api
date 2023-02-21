bb99f4b05ef1447595c846610d7bcf9423507bcc
Leonardo Luan <leonardoluan2017@gmail.com>
2023-02-21 18:23:43 -0300

    :sparkles: feat: Inclusão de novas classes

--- src/main/java/com/luan/luanfood/domain/model/Grupo.java
+++ src/main/java/com/luan/luanfood/domain/model/Grupo.java
@@ -1,10 +1,16 @@
 package com.luan.luanfood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +18,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Estado {
+public class Grupo {
 
     @EqualsAndHashCode.Include
     @Id
@@ -21,5 +27,10 @@ public class Estado {
 
     @Column(nullable = false)
     private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
 
 }
--- src/main/java/com/luan/luanfood/domain/model/Produto.java
+++ src/main/java/com/luan/luanfood/domain/model/Produto.java
@@ -1,10 +1,14 @@
 package com.luan.luanfood.domain.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +16,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Estado {
+public class Produto {
 
     @EqualsAndHashCode.Include
     @Id
@@ -21,5 +25,18 @@ public class Estado {
 
     @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
 
 }
