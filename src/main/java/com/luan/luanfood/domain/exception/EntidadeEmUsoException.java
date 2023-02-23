9c66659f1cd75698ff7d9b730c202edfa95882ce
Leonardo Luan <leonardoluan2017@gmail.com>
2023-02-23 11:25:57 -0300

    :recycle: refactor: Refatorando as Exceptions

--- src/main/java/com/luan/luanfood/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/com/luan/luanfood/domain/exception/EntidadeEmUsoException.java
@@ -1,11 +1,13 @@
 package com.luan.luanfood.domain.exception;
 
-public class EntidadeEmUsoException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class EntidadeEmUsoException extends NegocioException {
 
     private static final long serialVersionUID = 1L;
 
     public EntidadeEmUsoException(String mensagem) {
         super(mensagem);
     }
-
 }
