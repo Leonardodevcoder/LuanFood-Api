c11961c99b731cda8387ddbc2631d45087c6ff77
Leonardo Luan <leonardoluan2017@gmail.com>
2023-02-23 13:11:45 -0300

    :sparkles: feat: Adicionando novas exceptions

---
+++ src/main/java/com/luan/luanfood/api/exceptionhandler/Problem.java
@@ -0,0 +1,19 @@
+package com.luan.luanfood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Builder
+public class Problem {
+
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+
+}
